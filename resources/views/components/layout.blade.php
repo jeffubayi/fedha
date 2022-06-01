163be6a80b1fe7c7773e196221c7a937981531ac
jeffubayi <jeffubayi@gmail.com>
2022-06-01 21:53:14 +0300

    add login UI

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -49,7 +49,7 @@
                 <ul class="space-y-2 tracking-wide mt-8">
                     <li>
                         <a href="/" aria-label="dashboard"
-                            class="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400">
+                            class="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-amber-500 to-amber-300">
                             <svg class="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
                                 <path
                                     d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z"
@@ -76,25 +76,6 @@
                             <span class="group-hover:text-gray-700">Students</span>
                         </a>
                     </li>
-                    {{-- <li>
-            <a href="/" class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path class="fill-current text-gray-300 group-hover:text-cyan-300" fill-rule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clip-rule="evenodd" />
-                <path class="fill-current text-gray-600 group-hover:text-cyan-600" d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
-            </svg>
-                <span class="group-hover:text-gray-700">Staff</span>
-            </a>
-        </li>
-        
-            <li>
-                <a href="/" class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path class="fill-current text-gray-300 group-hover:text-cyan-300" fill-rule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clip-rule="evenodd" />
-                        <path class="fill-current text-gray-600 group-hover:text-cyan-600" d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
-                    </svg>
-                    <span class="group-hover:text-gray-700">Subject</span>
-                </a>
-            </li> --}}
                     <li>
                         <a href="/listing/manage"
                             class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
