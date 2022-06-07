773a8e380413de51f4bb1e116d099fafc3d2e831
jeffubayi <jeffubayi@gmail.com>
2022-06-07 17:16:44 +0300

    update password verifaction  page

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,30 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
                         @csrf
 
+                        <div class="text-center ">
+            
+                            <h5 class="text-lg mt-1 font-medium text-gray-900 ">Confirm password reset </h5>
+                            </div>
+
                         <input type="hidden" name="token" value="{{ $token }}">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                            <input type="email" name="email" id="email"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  
+                                 required="" value="{{ old('email') }}" />
+                            @error('email')
+                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
                         </div>
 
                         <div class="form-group row">
@@ -39,6 +34,12 @@
                                     </span>
                                 @enderror
                             </div>
+                            <div>
+                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Your password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                                    required="">
+                            </div>
                         </div>
 
                         <div class="form-group row">
@@ -49,17 +50,12 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
+            
+                        <button type="submit"
+                        class="w-full text-white bg-blue-600  hover:bg-iamber-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">{{ __('Reset Password') }}</button>
+                    <div class="text-sm font-medium text-gray-500 ">
+                        Not registered? <a href="/register" class="text-blue-600 hover:underline ">Create account</a>
+                    </div>
                     </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
 @endsection
