b80af2a1659ae42a2b436004759d3fd1b25e5a72
jeffubayi <jeffubayi@gmail.com>
2022-06-08 16:26:25 +0300

    update home controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,9 +23,10 @@ class HomeController extends Controller
     public function index()
     {
 
-        $data['incomes'] = Income::where('user_id', Auth::User()->id)->whereYear('income_date', Carbon::now()->year)->whereMonth('income_date', Carbon::now()->month)->sum('income_amount');
-        $data['expenses'] = Expense::where('user_id', Auth::User()->id)->whereYear('expense_date', Carbon::now()->year)->whereMonth('expense_date', Carbon::now()->month)->sum('expense_amount');
+        $data['incomes'] = Income::where('user_id', Auth::User()->id)->sum('income_amount');
+        $data['expenses'] = Expense::where('user_id', Auth::User()->id)->sum('expense_amount');
         $data['balance'] = $data['incomes'] - $data['expenses'];
+        $data['latests']= Expense::where('user_id', Auth::user()->id)->latest()->paginate(5);
 
         return view('pages.dashboard', $data);
     }
